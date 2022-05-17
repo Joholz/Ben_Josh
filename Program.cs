a1227694b755e3a271c968eb95443252e0270b94
BJensen1221 <bro.benjensen@gmail.com>
2022-05-17 12:24:43 -0400

    Ben commit

--- Program.cs
+++ Program.cs
@@ -8,6 +8,14 @@ namespace ConsoleApp_Dev
         {
             Console.WriteLine("Hello World!");
 
+            Console.WriteLine();
+
+            Console.WriteLine("My name is Josh");
+
+            Console.WriteLine();
+
+            Console.WriteLine("I am the best coder of all time");
+
             //test commit from laptop
             Console.ReadKey(); //this is used to pause the program...
                                //wait for us to tell the program its ok to close whenever we input a character
